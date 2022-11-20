11bf4789ce25eebfea7dc248498bc8353380ab37
mayankkuhaad <117817131+mayankkuhaad@users.noreply.github.com>
2022-11-21 01:03:44 +0530

    files uploaded

---
+++ src/index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+const bodyParser = require("body-parser");
+const port = 8080
+app.use(express.urlencoded());
+
+// Parse JSON bodies (as sent by API clients)
+app.use(express.json());
+
+
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.use(bodyParser.json())
+// your code goes here
+
+app.get('/', (req,res)=>{
+    res.send("hello World!");
+})
+
+
+app.listen(port, () => console.log(`App listening on port ${port}!`))
+
+module.exports = app;
